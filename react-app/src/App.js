9ff85508671791c49ec5316944ed667517170f74
woojin <zmfpdl64@naver.com>
2023-03-07 15:22:57 +0900

    react생성 and build 생성

---
+++ react-app/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>hello React</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
