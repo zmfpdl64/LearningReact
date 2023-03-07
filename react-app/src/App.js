6862b2eb6f1d19128f633ad54240c29728cad6a2
woojin <zmfpdl64@naver.com>
2023-03-07 16:15:19 +0900

    제목, 선택바, 내용, 푸터 컴포넌트화

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,11 +7,50 @@ class Subject extends Component {
       <header>
         <h1>Web</h1>
         world wide web!
-    </header>
+      </header>
     );
   }
 }
 
+class TOC extends Component{
+  render() {
+    return(
+      <nav>
+        <ul>
+          <li><a href="1.html">HTML</a></li>
+          <li><a href="2.html">CSS</a></li>
+          <li><a href="3.html">JavaScript</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component{
+  render() {
+    return (
+      <article>
+        <h2>HTML</h2>
+        HTML HyperText Markup Language.
+      </article>
+    );
+  }
+}
+
+class Footer extends Component{
+  render() {
+    return (
+      <div class="row">
+        <div class="col-2"></div>
+        <div class="col-8">
+          <h1>i'm Footer</h1>
+        </div>
+        <div class="col-2"></div>
+
+      </div>
+    )
+  }
+}
 
 
 class App extends Component {
@@ -19,6 +58,9 @@ class App extends Component {
     return (
       <div className="App">
         <Subject></Subject>
+        <TOC></TOC>
+        <Content></Content>
+        <Footer></Footer>
       </div>
     );
   }
