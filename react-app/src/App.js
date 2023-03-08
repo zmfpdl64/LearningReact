517ca1ba26701aa2040105d6e1671a25b37366b0
woojin <zmfpdl64@naver.com>
2023-03-08 16:34:08 +0900

    state로 필드 주입 방식을 변경해주었다.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,14 +9,33 @@ import Footer from './components/Footer';
 
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject:{title:"Web", sub:"Wolrd wide Web"},
+      content: {
+        title:"HTML",
+        desc: "HTML HyperText Markup Language."
+      },
+      footer:{
+        msg: "i'm Footer"
+      }
+
+    }
+
+  }
+
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <Subject title="React" sub="For UI"></Subject>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}>
+        </Subject>
         <TOC></TOC>
-        <Content title="HTML" desc="HTML HyperText Markup Language."></Content>
-        <Footer msg="i'm Footer"></Footer>
+        <Content title={this.state.content.title} desc={this.state.content.desc}></Content>
+        <Footer msg={this.state.footer.msg}></Footer>
       </div>
     );
   }
